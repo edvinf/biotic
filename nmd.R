a14cbb0e9b2ebad036f6bb3e9e26085b63a7d481
edvinf <edvin.fuglebakk@gmail.com>
2017-04-05 14:57:45 +0200

    Changed example to saving year file to excel

--- nmd.R
+++ nmd.R
@@ -32,74 +32,21 @@ load_biotic_by_serial_year <- function(serial_lower, serial_upper, year, dir, fi
   }
 }
 
-
-#
-#
-# Example of application
-#
-#
-
-#' Parses to a flat table all data for a given list of species, for range of serial numbers and a range of years.
-#' @param serial_lower first serial number to include
-#' @param serial_upper last serial number to include
-#' @param year_lower first year to include
-#' @param year_upper last year to include
-#' @param specieslist list of tns codes to include
-#' @param handlers passed to parse_biotic
-#' @param chunks Denotes how many download to split the serial number range in. Total downloads: chunks*(year_upper-year_lower+1)
-#' @return Tibble with all data as flat file.
-filter_by_species <- function(serial_lower, serial_upper, year_lower, year_upper, specieslist, handlers=biotic_1_4_handlers, chunks = 20){
-  if (year_lower > year_upper){
-    stop("Illegal range of years")
+#' Downloads all biotic data for a given year from NMD and saves as excel file
+#' @param excelfile name of target excelfile. Will be overwritten if already existing.
+#' @param year 
+#' @param tempdir location for storing temp-data (existing files will be overwritten, but will not be automatically deleted after completion)
+save_year_to_excel <- function(excelfile, year, tempdir){
+  require(openxlsx)
+  source("biotic.R")
+  maxs=99999
+  bio_xml <- load_biotic_by_serial_year(0, maxs, year, tempdir, overwrite=T)
+  if (is.null(bio_xml)){
+    stop("Data could not be downloaded.")
   }
-  source("/Users/a5362/code/github/hi_biotic_parser/biotic.R")
-  source("/Users/a5362/code/github/hi_biotic_parser/xml_filter.R")
-  tmpdir <- "/Users/a5362/t"
-  tmp_file <- "tmp.xml"
-  filtered_file <- "tmp_filtered.xml"
-  
-  flat <- NULL
-  stepsize = ceiling((serial_upper-serial_lower)/chunks)
-  for (y in year_lower:year_upper){
-    for (s in seq(serial_lower, serial_upper, stepsize)){
-      xml <- load_biotic_by_serial_year(s, min(s+stepsize, serial_upper), y, tmpdir, tmp_file, T)
-      if (!is.null(xml)){
-        apply_xml_filters(xml, filtered_file, c(function(tree){retain_species_catch(tree, specieslist)}, drop_childless_stations, drop_childless_missions))
-        bioticdata <- parse_biotic(filtered_file, handlers=handlers)
-        cflat <- flatten(bioticdata)
-        if (is.null(flat)){
-          flat <- cflat        
-        }
-        else{
-          flat <- bind_rows(flat, cflat)        
-        }
-      }
-    }
-  }
-  return(flat)
+  tab <- flatten(parse_biotic(bio_xml))  
+  write.xlsx(tab, excelfile, overwrite=T)
 }
-
-
-example_blb <- function(){
-  specieslist <- c("164740", "164760", "164760") #brosme, lange, blålange
-  ss <- 86000
-  se <- 87000
-  ys <- 2015
-  ye <- 2016
-  path <- "/Users/a5362/t"
-  data <- filter_by_species(ss,se,ys,ye, specieslist, handlers=biotic_1_4_handlers[c("mission", "fishstation", "catchsample", "individual", "agedetermination")])
-  if (nrow(data)==0){
-    stop("No matches found")
-  }
-  filename_xls <- paste(path, paste(paste(specieslist, collapse="_"), ".xlsx", sep=""), sep="/")
-  filename_csv <- paste(path, paste(paste(specieslist, collapse="_"), ".csv", sep=""), sep="/")
-  write.csv(data, filename_csv)
-  library(xlsx)
-  write.xlsx2(data, filename_xls)
-  return(data)
-}
-
-
 
 test <- function(){
   file <- load_biotic_by_serial_year(00000, 10000, 1990, "/Users/a5362/t", T)
@@ -108,5 +55,7 @@ test <- function(){
   print(file)
 }
 
-
+test_save_year_to_excel <- function(){
+  system.time(save_year_to_excel("test_ste.xlsx", 2015, "."))
+}
 
